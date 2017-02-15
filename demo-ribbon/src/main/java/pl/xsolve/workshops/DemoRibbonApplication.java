182bc04ff53c79b606f0491f848aff058728cf44
kamil.zielinski <kamil.zielinski@xsolve.pl>
2017-02-15 16:49:43 +0100

    added visibility of config server to eureka's server

--- demo-ribbon/src/main/java/pl/xsolve/workshops/DemoRibbonApplication.java
+++ demo-ribbon/src/main/java/pl/xsolve/workshops/DemoRibbonApplication.java
@@ -2,15 +2,15 @@ package pl.xsolve.workshops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import pl.xsolve.workshops.config.RibbonConfiguration;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RibbonClient(name = "ribbon-server", configuration = RibbonConfiguration.class)
 public class DemoRibbonApplication {
 
--- demo-ribbon/src/main/java/pl/xsolve/workshops/controller/TestController.java
+++ demo-ribbon/src/main/java/pl/xsolve/workshops/controller/TestController.java
@@ -8,11 +8,22 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TestController {
 
+    private static final String HELLO_PATH = "/hello";
+    private static final String REFRESH_PATH = "/refresh";
+
+    private static final String BOOTY_HELLO_PATH = "http://booty/hello";
+    private static final String CONFIG_SERVER_PATH = "http://config-server/refresh";
+
     @Autowired
     protected RestTemplate restTemplate;
 
-    @RequestMapping("/hi")
-    public String hi() {
-        return restTemplate.getForObject("http://booty/hello", String.class);
+    @RequestMapping(HELLO_PATH)
+    public String hello() {
+        return restTemplate.getForObject(BOOTY_HELLO_PATH, String.class);
+    }
+
+    @RequestMapping(REFRESH_PATH)
+    public String refresh() {
+        return restTemplate.getForObject(CONFIG_SERVER_PATH, String.class);
     }
 }
